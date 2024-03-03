6383fee35bd3de8607a702720a2bcc0bde03c5c7
ogeeDeveloper <omarogrant202@hotmail.com>
2024-03-03 13:29:13 -0500

    Created the comand and handler for creating a new Leave Type

---
+++ Application/Features/LeaveType/Commands/CreateLeaveTypeCommand/CreateLeaveTypeCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveTypeCommand
+{
+    public record CreateLeaveTypeCommand(string Name, int DefaultDays) : IRequest<int>;
+}
---
+++ Application/Features/LeaveType/Commands/CreateLeaveTypeCommand/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveTypeCommand
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // Validate incoming data
+
+            // Convert to domain entity type
+            var leaveTypeToBeCreated = _mapper.Map<Domain.LeaveType>(request);
+
+            // Save to the database
+            await _leaveTypeRepository.CreateAsync(leaveTypeToBeCreated);
+            // Return the id of the newly reated Leave Type
+            return leaveTypeToBeCreated.Id;
+        }
+    }
+}
