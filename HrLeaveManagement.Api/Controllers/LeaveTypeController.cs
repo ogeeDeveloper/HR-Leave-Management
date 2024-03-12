8e20ca8387d7824f40003efa6a669e4e9b5c16ac
ogeeDeveloper <omarogrant202@hotmail.com>
2024-03-11 21:18:55 -0500

    Created the leaveType controller and added the Get request to retrieve all the leave types

---
+++ HrLeaveManagement.Api/Controllers/LeaveTypeController.cs
@@ -0,0 +1,25 @@
+﻿using HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypeQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HrLeaveManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LeaveTypeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public LeaveTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    // Get all leavetype
+    [HttpGet]
+    public async Task<List<LeaveTypeDto>> GetLeaveTypes()
+    {
+        // get leave types using the mediatr to send the request
+        var leaveTypes = await _mediator.Send(new GetAllLeaveTypeQuery());
+        return leaveTypes;
+    }
+}
