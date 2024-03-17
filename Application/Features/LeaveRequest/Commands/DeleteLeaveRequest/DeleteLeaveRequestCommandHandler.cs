7b28124942789fa4a5ebf77dadd9b9d632143ea7
ogeeDeveloper <omarogrant202@hotmail.com>
2024-03-17 11:22:57 -0500

    Added feature to delete a leave request

---
+++ Application/Features/LeaveRequest/Commands/DeleteLeaveRequest/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest;
+
+public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand, Unit>
+{
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+    public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+    }
+    
+    public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+        if (leaveRequest is null)
+            throw new NotFoundException(nameof(LeaveRequest), request.Id);
+
+        await _leaveRequestRepository.DeleteAsync(leaveRequest);
+        
+        return Unit.Value;
+    }
+}
