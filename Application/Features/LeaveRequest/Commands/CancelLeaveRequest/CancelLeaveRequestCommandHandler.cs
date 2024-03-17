dc6ec5d912e08ca9a730cc5b2b82c40770dd11d7
ogeeDeveloper <omarogrant202@hotmail.com>
2024-03-17 13:15:50 -0500

    Implemented feature to cancel leave request

---
+++ Application/Features/LeaveRequest/Commands/CancelLeaveRequest/CancelLeaveRequestCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using HRLeaveManagement.Application.Contracts.Email;
+using HRLeaveManagement.Application.Contracts.Logging;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Exceptions;
+using HRLeaveManagement.Application.Models.Email;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest;
+
+public class CancelLeaveRequestCommandHandler : IRequestHandler<CancelLeaveRequestCommand, Unit>
+{
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly IEmailSender _emailSender;
+    private readonly IAppLogger<CancelLeaveRequestCommandHandler> _appLogger;
+
+    public CancelLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IEmailSender emailSender, IAppLogger<CancelLeaveRequestCommandHandler> appLogger)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _emailSender = emailSender;
+        _appLogger = appLogger;
+    }
+    
+    public async Task<Unit> Handle(CancelLeaveRequestCommand request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+        if (leaveRequest is null)
+            throw new NotFoundException(nameof(leaveRequest), request.Id);
+
+        leaveRequest.Cancelled = true;
+        
+        // Re-evaluate the employee's allocation for the leave type
+
+        try
+        {
+            var email = new EmailMessage
+            {
+                To = string.Empty,
+                Body = $"Your leave request has been cancelled",
+                Subjecct = "Cancellation successful."
+            };
+
+            await _emailSender.SendEmil(email);
+        }
+        catch (Exception ex)
+        {
+            _appLogger.LogInformation(ex.Message);
+        }
+        
+        return Unit.Value;
+    }
+}
